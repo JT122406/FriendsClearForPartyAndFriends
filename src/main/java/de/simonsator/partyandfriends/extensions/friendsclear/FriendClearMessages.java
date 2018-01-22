b94787d2001a84ebb95a8c89f80c279de7af9f2a
Simonsator <00pflaume@gmail.com>
2018-01-22 14:07:53 +0100

    Added more messages

--- src/main/java/de/simonsator/partyandfriends/extensions/friendsclear/FriendClearMessages.java
+++ src/main/java/de/simonsator/partyandfriends/extensions/friendsclear/FriendClearMessages.java
@@ -17,7 +17,10 @@ public class FriendClearMessages extends LanguageConfiguration {
 
 	private void loadDefaults() {
 		set("CommandUsage", "&8/&5friend clear <confirmation key> &8- &7Removes all your friends");
-		set("EnterPreviousGeneratedKey", "");
+		set("ANewConfirmationKeyWasGenerated", " &7A new confirmation key was generated. The key is &e[CONFIRMATION_KEY]&7. Please type into the console &6/friend clear [CONFIRMATION_KEY] &7to confirm the removing of all your friends. If a key was generated previously it is now invalid");
+		set("IncorrectConfirmationKey", " &7The confirmation key you entered was incorrect. Please try again entering your confirmation key or generate a new one by using &6/friend clear&7.");
+		set("AllFriendsRemoved", " &7All your friends were removed from your friend list.");
+		set("NoConfirmationKeyGeneratedYet", " &7No confirmation key was created yet. Please use &6/friend clear &7to generate one&7.");
 	}
 
 	@Override
